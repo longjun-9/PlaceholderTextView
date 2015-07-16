8a7465df94c4c0a2df17c6f14720b30727e4109f
longjun <longjun9@gmail.com>
2015-07-16 14:59:07 +0800

    注释修改

--- PlaceholderTextView/main.m
+++ PlaceholderTextView/main.m
@@ -1,9 +1,8 @@
 //
 //  main.m
-//  PlaceholderTextView
 //
-//  Created by gitBurning on 14/12/8.
-//  Copyright (c) 2014年 gitBurning. All rights reserved.
+//  Created by longjun on 15/7/16.
+//  Copyright (c) 2014年 longjun. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
